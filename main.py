2c1bd7ada10d21fc120295214e4335d23b0b7ce3
LukaLondaridzen <tactusglacies@gmail.com>
2025-10-27 20:37:14 +0400

    added: main.py file

---
+++ main.py
@@ -0,0 +1,28 @@
+import csv
+
+#
+# data = [
+#     {'first_name': "saxeli", "last_name": "gvari", "age": 20},
+#     {'first_name': "1saxeli", "last_name": "gvari", "age": 30},
+#     {'first_name': "3saxeli", "last_name": "gvari", "age": 23},
+#     {'first_name': "2saxeli", "last_name": "gvari", "age": 50},
+#     {'first_name': "4saxeli", "last_name": "gvari", "age": 21},
+#
+# ]
+# with open('name.csv', "w", newline='') as f:
+#     writer = csv.DictWriter(f, fieldnames=['first_name', 'last_name', 'age'])
+#     writer.writeheader()
+#     writer.writerows(data)
+
+def sort_info(person):
+    return int(person['age'])
+
+
+with open('name.csv', 'r') as f:
+    reader = csv.DictReader(f)
+    s_lst = sorted(reader, key=sort_info)
+
+with open('new_name.csv', 'w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=['first_name', 'last_name', 'age'])
+    writer.writeheader()
+    writer.writerows(s_lst)
